eceb867b9722958aeafedc83c60d280ef419f912
Prateek <prateeksanodiya937@gmail.com>
2022-01-16 20:34:39 +0530

    asdlfj

--- Learning_CS/pool.py
+++ Learning_CS/pool.py
@@ -1,5 +1,5 @@
 import pymysql as MYSQL
 def connectionPooling():
-    db=MYSQL.connect(host='localhost',port=3306,user='root',passwd='Pratik@123',db='learning')
+    db=MYSQL.connect(host='ec2-34-230-198-12.compute-1.amazonaws.com',port=5432,user='dhaiducohaslbx',passwd='9942775b2b7ed53767e511bf6fb8eedfb3b75f0f888ee1fac5f8f9fe3a7ed4dc',db='d20lptt31kgtp1')
     cmd=db.cursor()
     return db,cmd
